d1258381f3f99538bf7f1c0c175a21c59d5218d1
Letícia Peixoto <leticia_95lima@hotmail.com>
2025-05-19 22:01:32 -0300

    add 4

---
+++ 4.py
@@ -0,0 +1,10 @@
+name_produto = "tênis"
+preco_unitario = 200.00
+qnt_adquirida = 3
+
+preco_total = preco_unitario * qnt_adquirida
+
+print("Nome do produto:", name_produto)
+print("Preço unitário: R$", preco_unitario)
+print("Quantidade:", qnt_adquirida)
+print("Preço total: R$", preco_total)
