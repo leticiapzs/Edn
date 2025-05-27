e330f82cd0d6c3eeb1d40d6f77a0418601784490
Letícia Peixoto <leticia_95lima@hotmail.com>
2025-05-27 19:13:37 -0300

    atividade pratica 03

---
+++ classificador_idade.py
@@ -0,0 +1,11 @@
+idade = int(input("Digite sua idade: "))
+
+
+if idade <= 12:
+  print("Você é Criança.")
+elif idade <= 17:
+  print("Você é Adolescente.")
+elif idade <= 59:
+  print("Você é Adulto.")
+else:
+  print("Você é Idoso.")
